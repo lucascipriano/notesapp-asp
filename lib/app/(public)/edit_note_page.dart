1bf9b27d5f71cee8480d4e7549506e8a3def1daa
Lucas Cipriano <lucasncborges@gmail.com>
2024-10-19 16:27:59 -0300

    dialog confirm delet

--- lib/app/(public)/edit_note_page.dart
+++ lib/app/(public)/edit_note_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/app/interactor/actions/notes_actions.dart';
 import 'package:notesapp/app/interactor/models/notes_model.dart';
+import 'package:routefly/routefly.dart';
 
 class EditNotePage extends StatelessWidget {
   final NotesModel note;
@@ -32,9 +33,14 @@ class EditNotePage extends StatelessWidget {
             icon: const Icon(Icons.save),
           ),
           IconButton(
-            onPressed: () {
-              deleteAction(note.id);
-              Navigator.pop(context);
+            onPressed: () async {
+              // Chama o _alertDelet e espera o retorno da confirmação
+              final confirm = await _alertDelet(context, note.id);
+
+              // Verifica se o retorno foi true para executar a ação desejada
+              if (confirm == true) {
+                Routefly.pop(context);
+              }
             },
             icon: const Icon(Icons.delete),
           ),
@@ -72,4 +78,30 @@ class EditNotePage extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<bool?> _alertDelet(BuildContext context, int noteId) async {
+  final confirm = await showDialog<bool>(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: const Text('Confirm Deletion'),
+      content: const Text('Are you sure you want to delete this note?'),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            // Fecha o diálogo antes da ação
+            Navigator.of(context).pop(true);
+            deleteAction(noteId);
+          },
+          child: const Text('Delete'),
+        ),
+      ],
+    ),
+  );
+
+  return confirm; // Retorna true ou false dependendo da ação
 }
