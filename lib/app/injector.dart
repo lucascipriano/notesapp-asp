1dc1e6360c0ce94cc2fcbf372ac8e190ae4c2c75
Lucas Cipriano <lucasncborges@gmail.com>
2024-10-22 20:20:49 -0300

    deletei algumas coisas

--- lib/app/injector.dart
+++ lib/app/injector.dart
@@ -1,12 +1,10 @@
 import 'package:auto_injector/auto_injector.dart';
 import 'package:notesapp/app/data/repositories/shared_todo_respostory.dart';
 import 'package:notesapp/app/interactor/repositories/note_repository.dart';
-import 'package:notesapp/app/theme/theme_manager.dart';
 
 final injector = AutoInjector();
 
 void registerInstances() {
   injector.add<NoteRepository>(SharedTodoRespostory.new);
-  injector.add<ThemeManager>(ThemeManager.new);
   injector.commit();
 }
