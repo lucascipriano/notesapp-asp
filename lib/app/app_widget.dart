796606ee5a5e50747f90caf0e23925db4e50647d
Lucas Cipriano <lucasncborges@gmail.com>
2024-10-22 20:23:08 -0300

    alteração para buscar o heme_constants

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/app/data/repositories/theme_nofier.dart';
 import 'package:notesapp/routes.g.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart'; // Para gerenciar o estado do tema
 import 'package:routefly/routefly.dart';
 
 class AppWidget extends StatefulWidget {
@@ -15,12 +15,12 @@ class _AppWidgetState extends State<AppWidget> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => ThemeManager(),
+      create: (_) => ThemeManager(), // Provedor do ThemeManager
       child: Consumer<ThemeManager>(
         builder: (context, themeManager, child) {
           return MaterialApp.router(
             debugShowCheckedModeBanner: false,
-            theme: themeManager.currentTheme,
+            theme: themeManager.currentTheme, // Use currentTheme aqui
             routerConfig: Routefly.routerConfig(
               routes: routes,
               initialPath: routePaths.splash,
